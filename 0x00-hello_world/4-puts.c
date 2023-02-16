6f718162112219a5c29f493639107c7d2a733a95
Kiyotaka9 <hopeakegbe9@gmail.com>
2023-02-16 07:54:01 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
+#!/bin/bash
 #include <stdio.h>
 
-	int main("\"Programming is like building a multilingual puzzle")
+	int main(void)
 	{
+		puts("\"Programming is like building a multilingual puzzle");
+	
     		return;
-}
+	}	
