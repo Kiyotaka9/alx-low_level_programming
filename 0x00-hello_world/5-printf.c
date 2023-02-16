e018a142732cfe2050bf33cfcecc481cc9678f6b
Kiyotaka9 <hopeakegbe9@gmail.com>
2023-02-16 08:54:44 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A C programming code that prints a line using puts as a function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
